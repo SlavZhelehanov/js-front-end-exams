aba035b00b2dd6e2ee25803534e3be4027a63ec4
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-05-26 17:41:14 +0300

    Create an Order

--- 01. JS Front-End Retake Exam - 19 December 2024/03.StockFlow/03.StockFlow_Resources/app.js
+++ 01. JS Front-End Retake Exam - 19 December 2024/03.StockFlow/03.StockFlow_Resources/app.js
@@ -32,3 +32,41 @@ loadOrders.addEventListener("click", async () => {
         `;
     }
 });
+orderBtn.addEventListener("click", async e => {
+    e.preventDefault();
+
+    const name = document.getElementById("name");
+    const date = document.getElementById("date");
+    const quantity = document.getElementById("quantity");
+
+    if (name.value != '' && date.value != '' && quantity.value != '') {
+        const postReuiest = await fetch("http://localhost:3030/jsonstore/orders", {
+            method: "POST",
+            header: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name: name.value, date: date.value, quantity: quantity.value })
+        });
+
+        if (!postReuiest.ok) {
+            const errMsg = await postReuiest.json();
+            throw new Error(errMsg.message);
+        }
+
+        name.value = '';
+        date.value = '';
+        quantity.value = '';
+
+        const data = await getData();
+
+        for (const order in data) {
+            list.innerHTML += `
+            <div class="container" id="${data[order]._id}">
+                <h2>${data[order].name}</h2>
+                <h3>${data[order].date}</h3>
+                <h3>${data[order].quantity}</h3>
+                <button class="change-btn">Change</button>
+                <button class="done-btn">Done</button>
+            </div>
+        `;
+        }
+    }
+});
