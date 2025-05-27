1afc2c6fb22530a0a9dc3cc37a53ce5713548bbe
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-05-27 13:36:51 +0300

    Task 1
    Getting the information from the form

--- 02. JS Front-End Regular Exam - 15 December 2024/02. Eco Events/02.Eco-Events/app.js
+++ 02. JS Front-End Regular Exam - 15 December 2024/02. Eco Events/02.Eco-Events/app.js
@@ -1,5 +1,38 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  //TODO
+    const email = document.getElementById("email");
+    const event = document.getElementById("event");
+    const location = document.getElementById("location");
+    const nextBtn = document.getElementById("next-btn");
+    const previewList = document.getElementById("preview-list");
+
+    nextBtn.addEventListener("click", e => {
+        e.preventDefault();
+
+        if (email.value != '' && event.value != '' && location.value != '') {
+            previewList.innerHTML = `<li class="application">
+                <article>
+                    <h4>${email.value}</h4>
+                    <p>
+                        <strong>Event:</strong>
+                        <br>
+                        ${event.value}
+                    </p>
+                    <p>
+                        <strong>Location:</strong>
+                        <br>
+                        ${location.value}
+                    </p>
+                </article>
+                <button class="action-btn edit">edit</button>
+                <button class="action-btn apply">apply</button>
+            </li>`;
+
+            nextBtn.disabled = true;
+            email.value = '';
+            event.value = '';
+            location.value = '';
+        }
+    });
 }
