bee3604ff1fb55532eeda472433ed25f424c8326
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-05-29 15:35:02 +0300

    Task 2: Getting the information from the form

--- 03. JS Front-End Retake Exam - 22 August 2024/02.Laptop Wishlist/02.Laptop-Wishlist/app.js
+++ 03. JS Front-End Retake Exam - 22 August 2024/02.Laptop Wishlist/02.Laptop-Wishlist/app.js
@@ -1,6 +1,33 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    //TODO...
-  }
-  
+    const addBtn = document.getElementById("add-btn");
+    const checkList = document.getElementById("check-list");
+
+    addBtn.addEventListener("click", e => {
+        e.preventDefault();
+
+        const laptopModel = document.getElementById("laptop-model");
+        const storage = document.getElementById("storage");
+        const price = document.getElementById("price");
+
+        if (laptopModel.value != "" && storage.value != "" && price.value != "") {
+            checkList.innerHTML = `
+                <li class="laptop-item">
+                    <article>
+                        <p>${laptopModel.value}</p>
+                        <p>Memory: ${storage.value} TB</p>
+                        <p>Price: ${price.value}$</p>
+                    </article>
+                    <button class="btn edit">edit</button>
+                    <button class="btn ok">ok</button>
+                </li>
+            `;
+
+            laptopModel.value = '';
+            storage.value = '';
+            price.value = '';
+            addBtn.disabled = true;
+        }
+    });
+}
