96964fa8a06f273dfd107e347c9f32d342b0dbcc
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-05-30 12:38:12 +0300

    Task 3: Load Appointments

--- 03. JS Front-End Retake Exam - 22 August 2024/03.Car Maintenance Booking/03.Car-Maintenance-Booking/app.js
+++ 03. JS Front-End Retake Exam - 22 August 2024/03.Car Maintenance Booking/03.Car-Maintenance-Booking/app.js
@@ -1,1 +1,30 @@
-//TODO...
+const API_URL = "http://localhost:3030/jsonstore/appointments/";
+const appointmentsList = document.getElementById("appointments-list");
+const loadAppointmentsBtn = document.getElementById("load-appointments");
+
+async function fetchAllAppointments() {
+    try {
+        const response = await fetch(API_URL);
+
+        if (!response.ok) {
+            const msg = await response.json();
+            throw new Error(msg.message);
+        }
+
+        const data = await response.json();
+
+        for (const [key, { date, model, service }] of Object.entries(data)) {
+            appointmentsList.innerHTML += `
+            <li class="appointment">
+                <h2>${model}</h2>
+                <h3>${date}</h3>
+                <h3>${service}</h3>
+            </li>
+        `;
+        }
+    } catch (error) {
+        console.error(error.message);        
+    }
+}
+
+loadAppointmentsBtn.addEventListener("click", fetchAllAppointments);
