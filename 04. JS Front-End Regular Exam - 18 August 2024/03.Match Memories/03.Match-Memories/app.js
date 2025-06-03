5c2c1a0026be8630735e0dc6e50edcebbfb99cb2
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-03 13:08:54 +0300

    Task 3: Add a Match

--- 04. JS Front-End Regular Exam - 18 August 2024/03.Match Memories/03.Match-Memories/app.js
+++ 04. JS Front-End Regular Exam - 18 August 2024/03.Match Memories/03.Match-Memories/app.js
@@ -1,6 +1,11 @@
 const API_URL = "http://localhost:3030/jsonstore/matches/";
 
 const list = document.getElementById("list");
+const host = document.getElementById("host");
+const guest = document.getElementById("guest");
+const score = document.getElementById("score");
+const addMatch = document.getElementById("add-match");
+const editMatch = document.getElementById("edit-match");
 
 async function fetchAllMatches() {
     try {
@@ -34,3 +39,26 @@ async function fetchAllMatches() {
 }
 
 document.getElementById("load-matches").addEventListener("click", fetchAllMatches);
+
+addMatch.addEventListener("click", async e => {
+    e.preventDefault();
+
+    if (host.value != '' && guest.value != '' && score.value != '') {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: { "Contet-Type": "application/json" },
+            body: JSON.stringify({ host: host.value, guest: guest.value, score: score.value })
+        });
+
+        host.value = '';
+        guest.value = '';
+        score.value = '';
+
+        if (!response.ok) {
+            const err = await response.json();
+            throw new Error(err.message);
+        }
+
+        await fetchAllMatches();
+    }
+});
