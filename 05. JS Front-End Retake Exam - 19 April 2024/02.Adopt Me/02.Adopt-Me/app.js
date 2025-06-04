73e671d3fa46c4a81651e05473868072b9156f46
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-04 19:25:35 +0300

    Task 1: Getting the information from the form

--- 05. JS Front-End Retake Exam - 19 April 2024/02.Adopt Me/02.Adopt-Me/app.js
+++ 05. JS Front-End Retake Exam - 19 April 2024/02.Adopt Me/02.Adopt-Me/app.js
@@ -1,6 +1,29 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    //TODO..
-  }
-  
+    const adoptBtn = document.getElementById("adopt-btn");
+    const type = document.getElementById("type");
+    const age = document.getElementById("age");
+    const gender = document.getElementById("gender");
+    const adoptionInfo = document.getElementById("adoption-info");
+
+    adoptBtn.addEventListener("click", e => {
+        e.preventDefault();
+
+        if (type.value != '' && age.value != '' && gender.value != '') {
+            adoptionInfo.innerHTML = `
+                <li>
+                    <article>
+                        <p>Pet:${type.value}</p>
+                        <p>Gender:${gender.value}</p>
+                        <p>Age:${age.value}</p>
+                    </article>
+                    <div class="buttons">
+                        <button class="edit-btn">Edit</button>
+                        <button class="done-btn">Done</button>
+                    </div>
+                </li>
+            `;
+        }
+    });
+}
