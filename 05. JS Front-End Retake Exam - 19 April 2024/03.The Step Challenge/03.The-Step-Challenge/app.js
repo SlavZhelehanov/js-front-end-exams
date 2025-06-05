7bf639d6d500aa4632457fa76cc661ef57177731
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-05 17:38:54 +0300

    Task 3: Load Records

--- 05. JS Front-End Retake Exam - 19 April 2024/03.The Step Challenge/03.The-Step-Challenge/app.js
+++ 05. JS Front-End Retake Exam - 19 April 2024/03.The Step Challenge/03.The-Step-Challenge/app.js
@@ -1,1 +1,37 @@
-//TODO...
+const API_URL = "http://localhost:3030/jsonstore/records/";
+
+const list = document.getElementById("list");
+const loadRrecords = document.getElementById("load-records");
+
+async function fetchData() {
+    try {
+        const response = await fetch(API_URL);
+
+        if (!response.ok) {
+            const msg = await response.json();
+            throw new Error(msg.message);
+        }
+
+        list.innerHTML = '';
+
+        const data = await response.json();
+
+        for (const [id, { calories, name, steps, _id }] of Object.entries(data)) {
+            list.innerHTML += `
+                <li class="record" id=${_id}>
+                    <div class="info">
+                        <p>${name}</p>
+                        <p>${steps}</p>
+                        <p>${calories}</p>
+                    </div>
+                    <div class="btn-wrapper">
+                        <button class="change-btn">Change</button>
+                        <button class="delete-btn">Delete</button>
+                    </div>
+                </li>
+            `;
+        }
+    } catch (error) { console.error(error); }
+}
+
+loadRrecords.addEventListener("click", fetchData);
