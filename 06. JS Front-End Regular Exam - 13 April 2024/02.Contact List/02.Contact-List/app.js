42d00d7edcb5f48b7d6278f26dbfb99f9d76fde4
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-06 19:14:55 +0300

    Task 2: Getting the information from the form

--- 06. JS Front-End Regular Exam - 13 April 2024/02.Contact List/02.Contact-List/app.js
+++ 06. JS Front-End Regular Exam - 13 April 2024/02.Contact List/02.Contact-List/app.js
@@ -1,6 +1,34 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    //...TODO
-  }
+    const addBtn = document.getElementById("add-btn");
+    const name = document.getElementById("name");
+    const phone = document.getElementById("phone");
+    const category = document.getElementById("category");
+    const checkList = document.getElementById("check-list");
+
+    addBtn.addEventListener("click", ev => {
+        ev.preventDefault();
+
+        if (name.value !== '' && phone.value !== '' && category.value !== '') {
+            checkList.innerHTML += `
+                <li>
+                    <article>
+                        <p>name: ${name.value}</p>
+                        <p>phone: ${phone.value}</p>
+                        <p>category: ${category.value}</p>
+                    </article>
+                    <div class="buttons">
+                        <button class="edit-btn"></button>
+                        <button class="save-btn"></button>
+                    </div>
+                </li>
+            `;
+
+            name.value = '' ;
+            phone.value = '';
+            category.value = '';
+        }
+    });
+}
   
