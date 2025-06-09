1ae41951f1bc7c3d8240dca9d53292aecb2a45cd
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-09 12:31:18 +0300

    Task 3: Load Games

--- 06. JS Front-End Regular Exam - 13 April 2024/03.My Board Games Collection/03.My-Board-Games-Collection/app.js
+++ 06. JS Front-End Regular Exam - 13 April 2024/03.My Board Games Collection/03.My-Board-Games-Collection/app.js
@@ -1,1 +1,30 @@
-//TODO...
+const API_URL = "http://localhost:3030/jsonstore/games/";
+
+const gamesList = document.getElementById("games-list");
+const loadGamesBtn = document.getElementById("load-games");
+
+let tempId;
+
+async function getGames() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    gamesList.innerHTML = '';
+    for (const [id, { name, players, type, _id }] of Object.entries(data)) {
+        gamesList.innerHTML += `
+        <div class="board-game" id="${_id}">
+            <div class="content">
+                <p>${name}</p>
+                <p>${players}</p>
+                <p>${type}</p>
+            </div>
+            <div class="buttons-container">
+                <button class="change-btn">Change</button>
+                <button class="delete-btn">Delete</button>
+            </div>
+        </div>
+        `;
+    }
+}
+
+loadGamesBtn.addEventListener("click", getGames);
