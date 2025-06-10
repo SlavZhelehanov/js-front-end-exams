603119f3be1c3c8a1bf27ca41528d71dd39a4512
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-10 13:18:26 +0300

    Task 2: Getting the information from the form

--- 07. JS Front-End Retake Exam -22 December 2023/02.Home Maintenance/02.Home Maintenance/app.js
+++ 07. JS Front-End Retake Exam -22 December 2023/02.Home Maintenance/02.Home Maintenance/app.js
@@ -1,3 +1,29 @@
 window.addEventListener("load", solve);
 
-//TODO...
+function solve() {
+    const [ place, action, person, addBtn] = document.querySelectorAll("input");
+    const taskList = document.querySelector("#task-list");
+
+    addBtn.addEventListener("click", e => {
+        e.preventDefault();
+
+        if(place.value !== '' && action.value !== '' && person.value !== '') {
+            taskList.innerHTML = `
+                <li class="clean-task">
+                    <article>
+                        <p>${place.value}</p>
+                        <p>${action.value}</p>
+                        <p>${person.value}</p>
+                    </article>
+                    <div class="buttons">
+                        <button class="edit">Edit</button>
+                        <button class="done">Done</button>
+                    </div>
+                </li>
+            `;
+            place.value = '';
+            action.value = '';
+            person.value = '';
+        }
+    })
+}
