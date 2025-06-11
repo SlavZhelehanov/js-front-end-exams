d58de3d8fc3e73e6aa9d7ef1d00fdb1e49dfb91d
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-11 12:49:15 +0300

    Task 3: Load Presents

--- 07. JS Front-End Retake Exam -22 December 2023/03.Christmas Gifts/03.Christmas-Gifts/app.js
+++ 07. JS Front-End Retake Exam -22 December 2023/03.Christmas Gifts/03.Christmas-Gifts/app.js
@@ -1,1 +1,28 @@
-//TODO...
+const API_URL = "http://localhost:3030/jsonstore/gifts/";
+
+const giftsList = document.getElementById("gift-list");
+const loadPresentsBtn = document.getElementById("load-presents");
+
+async function getGifts() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    giftsList.innerHTML = '';
+    for (const [id, { for: forWhom, gift, price, _id }] of Object.entries(data)) {
+        giftsList.innerHTML += `
+        <div class="gift-sock" id="${_id}">
+            <div class="content">
+                <p>${gift}</p>
+                <p>${forWhom}</p>
+                <p>${price}</p>
+            </div>
+            <div class="buttons-container">
+                <button class="change-btn">Change</button>
+                <button class="delete-btn">Delete</button>
+            </div>
+        </div>
+        `;
+    }
+}
+
+loadPresentsBtn.addEventListener("click", getGifts);
