6d11f4105f93f611ee0d9518c4725c77bfef060a
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-12 16:32:05 +0300

    Task 2: Get the information from the form

--- 08. JS Front-End Regular Exam - 16 Dec 2023/02.Expense Tracker/02.Expense-Tracker/app.js
+++ 08. JS Front-End Regular Exam - 16 Dec 2023/02.Expense Tracker/02.Expense-Tracker/app.js
@@ -1,3 +1,32 @@
 window.addEventListener("load", solve);
 
-//TODO
+function solve() {
+    const [expense, amount, date] = document.querySelectorAll("input");
+    const addBtn = document.getElementById("add-btn");
+    const previewList = document.getElementById("preview-list");
+
+    addBtn.addEventListener("click", e => {
+        e.preventDefault();
+
+        if(expense.value !== '' && amount.value !== '' && date.value !== '') {
+            previewList.innerHTML += `
+                <li class="expense-item">
+                    <article>
+                        <p>Type: ${expense.value}</p>
+                        <p>Amount: ${amount.value}$</p>
+                        <p>Date: ${date.value}</p>
+                    </article>
+                    <div class="buttons">
+                        <button class="btn edit">edit</button>
+                        <button class="btn ok">ok</button>
+                    </div>
+                </li>    
+            `;
+
+            expense.value = '';
+            amount.value = '';
+            date.value = '';
+            addBtn.disabled = true;
+        }
+    });
+}
