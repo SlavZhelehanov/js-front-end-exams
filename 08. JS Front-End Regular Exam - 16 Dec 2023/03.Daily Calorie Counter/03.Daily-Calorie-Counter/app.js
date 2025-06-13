200eb27295c47fca5be60fc6311f798b1c787d89
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-13 16:58:23 +0300

    Task 3: Load Meals

--- 08. JS Front-End Regular Exam - 16 Dec 2023/03.Daily Calorie Counter/03.Daily-Calorie-Counter/app.js
+++ 08. JS Front-End Regular Exam - 16 Dec 2023/03.Daily Calorie Counter/03.Daily-Calorie-Counter/app.js
@@ -1,1 +1,29 @@
-//TODO
+const API_URL = "http://localhost:3030/jsonstore/tasks/";
+
+const loadMealsBtn = document.getElementById("load-meals");
+const addMealBtn = document.getElementById("add-meal");
+const editMealBtn = document.getElementById("edit-meal");
+const food = document.getElementById("food");
+const calories = document.getElementById("calories");
+const time = document.getElementById("time");
+const list = document.getElementById("list");
+
+async function getTasks() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    list.innerHTML = '';
+    for (const [id, { food, calories, time, _id }] of Object.entries(data)) {
+        list.innerHTML += `<div class="meal">
+              <h2>${food}</h2>
+              <h3>${time}</h3>
+              <h3>${calories}</h3>
+              <div id="meal-buttons">
+                <button class="change-meal">Change</button>
+                <button class="delete-meal">Delete</button>
+              </div>
+            </div>`;
+    }
+}
+
+loadMealsBtn.addEventListener("click", getTasks);
