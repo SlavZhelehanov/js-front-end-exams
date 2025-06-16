896bf5a32eeb584c3291a10a971d3dd37a373556
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-16 19:21:54 +0300

    Task 2: Getting the information from the form

--- 09. JS Front-End Retake Exam - 17 August 2023/02. Dart Scoring/02. Dart Scoring/app.js
+++ 09. JS Front-End Retake Exam - 17 August 2023/02. Dart Scoring/02. Dart Scoring/app.js
@@ -1,6 +1,33 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    //TODO
-  }
+    const addBtn = document.getElementById("add-btn");
+    const player = document.getElementById("player");
+    const score = document.getElementById("score");
+    const round = document.getElementById("round");
+    const sureList = document.getElementById("sure-list");
+
+    addBtn.addEventListener("click", e => {
+        e.preventDefault();
+
+        if(player.value !== '' && score.value !== '' && round.value !== '') {
+            sureList.innerHTML = `
+                <li class="dart-item">
+                    <article>
+                        <p>${player.value}</p>
+                        <p>Score: ${score.value}</p>
+                        <p>Round: ${round.value}</p>
+                    </article>
+                    <button class="btn edit">edit</button>
+                    <button class="btn ok">ok</button> 
+                </li>
+            `;
+
+            player.value = '';
+            score.value = '';
+            round.value = '';
+            addBtn.disabled = true;
+        }
+    });
+}
   
