bcbd5fb341587eefc9f993110009a2f755db38e5
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-17 21:44:14 +0300

    Task 3: Load History

--- 09. JS Front-End Retake Exam - 17 August 2023/03.Weather Tracker/03.Weather Tracker/app.js
+++ 09. JS Front-End Retake Exam - 17 August 2023/03.Weather Tracker/03.Weather Tracker/app.js
@@ -1,1 +1,28 @@
-//TODO...
+const API_URL = "http://localhost:3030/jsonstore/tasks/";
+
+const loadHistoryBtn = document.getElementById("load-history");
+const addBtn = document.getElementById("add");
+const input = document.getElementById("input");
+const list = document.getElementById("list");
+
+async function getTasks() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    list.innerHTML = '';
+
+    for (const [id, { location, temperature, date, _id }] of Object.entries(data)) {
+        list.innerHTML += `
+                    <div class="container" id="${_id}">
+                        <h2>${location}</h2>
+                        <h3>${date}</h3>
+                        <h3 id="celsius">${temperature}</h3>
+                        <div id="buttons-container">  
+                            <button class="change-btn">Change</button>
+                            <button class="delete-btn">Delete</button>
+                        </div>                        
+                    </div>`;
+    }
+}
+
+loadHistoryBtn.addEventListener("click", getTasks);
