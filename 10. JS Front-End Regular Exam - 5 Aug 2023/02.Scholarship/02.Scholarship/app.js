21d7515bfc14fd6fd73fca204a6b9e53c7cc499b
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-18 21:48:54 +0300

    Task 2.1:	Getting the information from the form

--- 10. JS Front-End Regular Exam - 5 Aug 2023/02.Scholarship/02.Scholarship/app.js
+++ 10. JS Front-End Regular Exam - 5 Aug 2023/02.Scholarship/02.Scholarship/app.js
@@ -1,7 +1,32 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    
-  
-  }
+    const nextBtn = document.getElementById("next-btn");
+    const previewList = document.getElementById("preview-list");
+    const candidatesList = document.getElementById("candidates-list");
+    const [student, university, score] = document.querySelectorAll("input");
+
+    nextBtn.addEventListener("click", e => {
+        e.preventDefault();
+
+        if(student.value !== '' && university.value !== '' && score.value !== '') {
+            previewList.innerHTML = `
+                <li class="application">
+                    <article>
+                        <h4>${student.value}</h4>
+                        <p>University: ${university.value}</p>
+                        <p>Score: ${score.value}</p>
+                    </article>
+                    <button class="action-btn edit">edit</button>
+                    <button class="action-btn apply">apply</button>  
+                </li>
+            `;
+
+            student.value = '';
+            university.value = '';
+            score.value = '';
+            nextBtn.disabled = true;
+        }
+    });
+}
   
