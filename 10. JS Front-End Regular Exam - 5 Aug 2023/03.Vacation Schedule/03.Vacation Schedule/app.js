807414f1bd7b9cda7d25fbf159e5d54ee011041b
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-19 17:30:50 +0300

    Task 3.1: Load Vacations

--- 10. JS Front-End Regular Exam - 5 Aug 2023/03.Vacation Schedule/03.Vacation Schedule/app.js
+++ 10. JS Front-End Regular Exam - 5 Aug 2023/03.Vacation Schedule/03.Vacation Schedule/app.js
@@ -1,1 +1,22 @@
-//TODO..
+const API_URL = "http://localhost:3030/jsonstore/tasks/";
+
+const loadVacations = document.getElementById('load-vacations');
+const list = document.getElementById('list');
+
+async function getTasks() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    list.innerHTML = '';
+    for (const [id, {date, days, name, _id}] of Object.entries(data)) {
+        list.innerHTML += `<div class="container">
+                        <h2>${name}</h2>
+                        <h3>${date}</h3>
+                        <h3>${days}</h3>
+                        <button class="change-btn">Change</button>
+                        <button class="done-btn">Done</button>
+                    </div>`;
+    }
+}
+
+loadVacations.addEventListener('click', getTasks);
