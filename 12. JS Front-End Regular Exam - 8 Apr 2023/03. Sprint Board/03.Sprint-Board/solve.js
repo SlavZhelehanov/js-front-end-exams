40d02b0a2fd4dbdb8509a3565b654e6598a1dc1e
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-27 13:33:44 +0300

    Task 3.1: Load the Board

--- 12. JS Front-End Regular Exam - 8 Apr 2023/03. Sprint Board/03.Sprint-Board/solve.js
+++ 12. JS Front-End Regular Exam - 8 Apr 2023/03. Sprint Board/03.Sprint-Board/solve.js
@@ -1,6 +1,56 @@
 // TODO:
 function attachEvents() {
+    const API_URL = "http://localhost:3030/jsonstore/tasks/";
 
+    const loadBoardBtn = document.getElementById('load-board-btn');
+    const todoSection = document.querySelector("#todo-section>ul");
+    const inProgressSection = document.querySelector("#in-progress-section>ul");
+    const codeReviewSection = document.querySelector("#code-review-section>ul");
+    const doneSection = document.querySelector("#done-section>ul");
+
+    async function getTasks() {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+
+        todoSection.innerHTML = "";
+        inProgressSection.innerHTML = "";
+        codeReviewSection.innerHTML = "";
+        doneSection.innerHTML = "";
+
+        for (const [id, {title, description, status, _id}] of Object.entries(data)) {
+            const li = document.createElement("li");
+
+            li.setAttribute("id", _id);
+            li.setAttribute("class", "task");
+            li.innerHTML = `<h3>${title}</h3><p>${description}</p>`;
+
+            switch (status) {
+                case "ToDo": {
+                    li.innerHTML += `<button>Move to In Progress</button>`;
+                    todoSection.appendChild(li);
+                    break;
+                }
+                case "In Progress": {
+                    li.innerHTML += `<button>Move to Code Review</button>`;
+                    inProgressSection.appendChild(li);
+                    break;
+                }
+                case "Code Review": {
+                    li.innerHTML += `<button>Move to Done</button>`;
+                    codeReviewSection.appendChild(li);
+                    break;
+                }
+                case "Done": {
+                    li.innerHTML += `<button>Close</button>`;
+                    doneSection.appendChild(li);
+                    break;
+                }
+                default: { break; }
+            }
+        }
+    }
+
+    loadBoardBtn.addEventListener("click", getTasks);
 }
 
 attachEvents();
