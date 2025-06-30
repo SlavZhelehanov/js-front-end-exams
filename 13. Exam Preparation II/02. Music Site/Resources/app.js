8929fc934cfd8aa9882a618bf77d5e98526ae14a
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-30 22:02:04 +0300

    Task 2.1: Getting the information about a new song

--- 13. Exam Preparation II/02. Music Site/Resources/app.js
+++ 13. Exam Preparation II/02. Music Site/Resources/app.js
@@ -1,5 +1,27 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    //TODO
+    const allHitsContainer = document.querySelector('#all-hits>.all-hits-container');
+    const [genre, name, author, date] = document.querySelectorAll('input');
+    const addBtn = document.querySelector('#add-btn');
+
+    addBtn.addEventListener('click', e => {
+        e.preventDefault();
+
+        if(genre.value !== '' && name.value !== '' && author.value !== '' && date.value !== '') {
+            allHitsContainer.innerHTML += `<div class="hits-info">
+            <h2>Genre: ${genre.value}</h2>
+            <h2>Name: ${name.value}</h2>
+            <h2>Author: ${author.value}</h2>
+            <h3>Date: ${date.value}</h3>
+            <button class="save-btn">Save song</button>
+            <button class="like-btn">Like song</button>
+            <button class="delete-btn">Delete</button>
+</div>`;
+            genre.value = '';
+            name.value = '';
+            author.value = '';
+            date.value = '';
+        }
+    });
 }
