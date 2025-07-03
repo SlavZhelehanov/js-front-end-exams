95d9c4b218bdcb160de48d3f42c0e03011c26b9b
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-07-03 16:13:45 +0300

    Task 3.1: Load Tasks

--- 14. Exam Preparation I/03. Todo List/03. Todo List/solve.js
+++ 14. Exam Preparation I/03. Todo List/03. Todo List/solve.js
@@ -1,6 +1,25 @@
 // TODO
 function attachEvents() {
-  
+    const API_URL = "http://localhost:3030/jsonstore/tasks/";
+
+    const loadButton = document.getElementById("load-button");
+    const todoList = document.getElementById("todo-list");
+
+    async function getTasks() {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+
+        todoList.innerHTML = "";
+
+        for (const [entry, { name, _id }] of Object.entries(data)) {
+            todoList.innerHTML += `<li><span>${name}</span><button>Remove</button><button>Edit</button></li>`;
+        }
+    }
+
+    loadButton.addEventListener("click", async event => {
+        event.preventDefault();
+        await getTasks();
+    });
 }
 
 attachEvents();
