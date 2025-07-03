061a9017fcae77174a0f2dad9f479b3d16c8fc80
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-07-03 16:17:07 +0300

    Task 3.2: Add Task

--- 14. Exam Preparation I/03. Todo List/03. Todo List/solve.js
+++ 14. Exam Preparation I/03. Todo List/03. Todo List/solve.js
@@ -3,7 +3,9 @@ function attachEvents() {
     const API_URL = "http://localhost:3030/jsonstore/tasks/";
 
     const loadButton = document.getElementById("load-button");
+    const addButton = document.getElementById("add-button");
     const todoList = document.getElementById("todo-list");
+    const title = document.getElementById("title");
 
     async function getTasks() {
         const response = await fetch(API_URL);
@@ -19,6 +21,21 @@ function attachEvents() {
     loadButton.addEventListener("click", async event => {
         event.preventDefault();
         await getTasks();
+    });
+
+    addButton.addEventListener("click", async event => {
+        event.preventDefault();
+
+        if (title.value !== '') {
+            await fetch(API_URL, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ name: title.value })
+            });
+
+            title.value = "";
+            await getTasks();
+        }
     });
 }
 
